51e2f508f600ec7cbd1425f19d1848d3a0371740
Kagisho <kagishomatlala@yahoo.com>
2023-02-22 21:28:35 +0200

    This is the 3rd commit from the javascript session

---
+++ index.js
@@ -0,0 +1,33 @@
+window.onload = function () {
+    let name = "Kagisho";
+    let surname = "Matlala";
+    let bio = "An aspiring software developer and web developer. I am passionate about coding and building digital solutions.";
+    let educationalBackground = "I hold a National Diploma in Information Technology from VUT";
+    let contactInformation = {
+        email: "kagishomatlala@yahoo.com",
+        phone: "071-234-5678",
+        address: "123 Rissik Str Midrand"
+    };
+
+    let projects = [
+        {
+            name: "Project 1",
+            description: "A web application for managing customer orders.",
+            technologies: ["JavaScript", "React", "Node.js", "MongoDB"],
+            url: "https://www.example.com/project1"
+        },
+        {
+            name: "Project 2",
+            description: "A mobile app for tracking daily fitness activities.",
+            technologies: ["Java", "Android SDK", "Firebase"],
+            url: "https://www.example.com/project2"
+        },
+        {
+            name: "Project 3",
+            description: "A data analysis tool for visualizing sales data.",
+            technologies: ["Python", "Pandas", "Matplotlib"],
+            url: "https://www.example.com/project3"
+        }
+    ];
+
+};
